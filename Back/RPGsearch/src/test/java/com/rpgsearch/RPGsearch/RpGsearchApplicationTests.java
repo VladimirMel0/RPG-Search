aabf94ad21671406381829571594d724cd998833
Vladimir Melo <vladimiraparecido3@gmail.com>
2023-01-25 19:54:15 -0300

    first commit

---
+++ Back/RPGsearch/src/test/java/com/rpgsearch/RPGsearch/RpGsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpgsearch.RPGsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpGsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
